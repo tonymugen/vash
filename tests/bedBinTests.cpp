6f4505bd573b9dfbe76f2a93a64487d82c1e4cca
Tony Greenberg <info@bayesicresearch.org>
2023-03-16 17:36:48 -0400

    finish and test the .bed binarization function

--- tests/bedBinTests.cpp
+++ tests/bedBinTests.cpp
@@ -30,69 +30,75 @@
 
 #include "random.hpp"
 
-void binarizeBedLocus(const size_t &bedIdx, const size_t &bedLocusSize, const std::vector<char> &bedLocus, const size_t &binIdx, const size_t &binLocusSize, BayesicSpace::RanDraw &prng, std::vector<uint8_t> &binLocus) {
-	constexpr size_t word64size{8};                              // size of uint64_t word in bytes
-	constexpr size_t word32size{4};                              // size of uint32_t word in bytes
-	constexpr auto wordMask{static_cast<uint64_t>(-word64size)}; // for rounding down to nearest divisible by 8 number 
-	constexpr uint64_t secondBitMask{0xaaaaaaaaaaaaaaaa};        // all bed second bits set
-	constexpr uint64_t firstBitMask{~secondBitMask};             // all bed first bits set
-	const size_t nEvenBedBytes{bedLocusSize & wordMask};         // number of bed bytes that fully fit into 64-bit words
+void binarizeBedLocus(const size_t &bedIdx, const size_t &bedLocusSize, const std::vector<char> &bedLocus, const size_t &binIdx, BayesicSpace::RanDraw &prng, std::vector<uint8_t> &binLocus) {
+	constexpr size_t word64size{8};                                                           // size of uint64_t word in bytes
+	constexpr size_t word32size{4};                                                           // size of uint32_t word in bytes
+	constexpr auto wordMask{static_cast<uint64_t>(-word64size)};                              // for rounding down to nearest divisible by 8 number 
+	constexpr uint64_t secondBitMask{0xaaaaaaaaaaaaaaaa};                                     // all bed second bits set
+	constexpr uint64_t firstBitMask{~secondBitMask};                                          // all bed first bits set
+	const size_t nEvenBedBytes{bedLocusSize & wordMask};                                      // number of bed bytes that fully fit into 64-bit words
 	size_t iBedByte{bedIdx};
 	size_t iBinByte{binIdx};
-	while (iBedByte < nEvenBedBytes){
+	while (iBedByte < nEvenBedBytes + bedIdx){
 		uint64_t bedWord{0};
 		memcpy(&bedWord, bedLocus.data() + iBedByte, word64size);
-		bedWord = ~bedWord;
 		auto binBits{static_cast<uint32_t>( _pext_u64(bedWord, firstBitMask) )};
 		const auto secondBedBits{static_cast<uint32_t>( _pext_u64(bedWord, secondBitMask) )};
 		const uint32_t maHoms{binBits & secondBedBits};
-		uint32_t allHets{(~maHoms) & binBits};     // set at all het positions
+		uint32_t allHets{(~maHoms) & secondBedBits};                                          // set at all het positions
 		const auto ranBits{static_cast<uint32_t>( prng.ranInt() )};
-		allHets &= ranBits;
-		binBits  = maHoms | allHets;
+		allHets &= ranBits;                                                                   // flip some of the het bits randomly
+		binBits  = maHoms | allHets;                                                          // add in the set het bits
 		memcpy(binLocus.data() + iBinByte, &binBits, word32size);
 		iBedByte += word64size;
 		iBinByte += word32size;
 	}
-	// add the trailing word
+	if (bedLocusSize > nEvenBedBytes){
+		const size_t nTrailingBedBytes{bedLocusSize - nEvenBedBytes};
+		const size_t nTrailingBinBytes{nTrailingBedBytes / 2 + (nTrailingBedBytes & 1)};
+		uint64_t bedWord{0};
+		memcpy(&bedWord, bedLocus.data() + iBedByte, nTrailingBedBytes);
+		auto binBits{static_cast<uint32_t>( _pext_u64(bedWord, firstBitMask) )};
+		const auto secondBedBits{static_cast<uint32_t>( _pext_u64(bedWord, secondBitMask) )};
+		const uint32_t maHoms{binBits & secondBedBits};
+		uint32_t allHets{(~maHoms) & secondBedBits};
+		const auto ranBits{static_cast<uint32_t>( prng.ranInt() )};
+		allHets &= ranBits;
+		binBits  = maHoms | allHets;
+		memcpy(binLocus.data() + iBinByte, &binBits, nTrailingBinBytes);
+	}
 }
 
 int main() {
-	constexpr size_t byteSize{8};                           // size of byte in bits
-	constexpr size_t llWordSize{8};                         // size of uint64_t word in bytes
-	constexpr size_t lWordInBits{32};                       // uint32_t size in bits
-	constexpr uint64_t secondBitMask{0xAAAAAAAAAAAAAAAA};
-	constexpr uint64_t firstBitMask{~secondBitMask};
+	constexpr size_t byteSize{8};
+	constexpr size_t nIndividuals{151};
+	constexpr size_t nLoci{6};
+	constexpr size_t bedLocusSize{nIndividuals / 4 + static_cast<size_t>( (nIndividuals % 4) > 0 )};
+	constexpr size_t binLocusSize{bedLocusSize / 2 + (bedLocusSize & 1)};
+	std::cout << bedLocusSize << " " << binLocusSize << "\n";
 	constexpr size_t seed{1711};
 	const std::string bedFile("bedConv.bed");
 	BayesicSpace::RanDraw prng(seed);
 	std::array<char, 3> magicBytes{0};
-	std::vector<char> bedBytes(llWordSize,0);
+	std::vector<char> bedBytes(bedLocusSize * nLoci, 0);
 	std::fstream bedIn;
 	bedIn.open(bedFile, std::ios::in | std::ios::binary);
 	bedIn.read( magicBytes.data(), magicBytes.size() );
-	bedIn.read(bedBytes.data(), llWordSize);
+	bedIn.read( bedBytes.data(), static_cast<std::streamsize>( bedBytes.size() ) );
 	bedIn.close();
-	uint64_t bedWord{0};
-	memcpy(&bedWord, bedBytes.data(), llWordSize);
-	bedWord = ~bedWord;
-	const auto firstBedBits{static_cast<uint32_t>( _pext_u64(bedWord, firstBitMask) )};
-	const auto secondBedBits{static_cast<uint32_t>( _pext_u64(bedWord, secondBitMask) )};
-	const uint32_t maHoms{firstBedBits & secondBedBits};
-	const uint32_t allHets{(~maHoms) & firstBedBits};     // set at all het positions
-	const auto ranBits{static_cast<uint32_t>( prng.ranInt() )};
-	std::cout << std::bitset<llWordSize * byteSize>(bedWord) << "\n";
-	std::cout << std::bitset<llWordSize * byteSize>(firstBitMask) << "\n";
-	std::cout << std::bitset<llWordSize * byteSize>( static_cast<uint64_t>(firstBedBits) ) << "\n\n";
-	std::cout << std::bitset<llWordSize * byteSize>(bedWord) << "\n";
-	std::cout << std::bitset<llWordSize * byteSize>(secondBitMask) << "\n";
-	std::cout << std::bitset<llWordSize * byteSize>( static_cast<uint32_t>(secondBedBits) ) << "\n\n";
-	std::cout << std::bitset<lWordInBits>(firstBedBits) << "\n";
-	std::cout << std::bitset<lWordInBits>(secondBedBits) << "\n";
-	std::cout << std::bitset<lWordInBits>(maHoms) << "\n";
-	std::cout << std::bitset<lWordInBits>(allHets) << "\n";
-	std::cout << std::bitset<lWordInBits>(ranBits) << "\n";
-	std::cout << std::bitset<lWordInBits>(allHets & ranBits) << "\n";
-	std::cout << std::bitset<lWordInBits>( maHoms | (allHets & ranBits) ) << "\n";
+	for (size_t iLocus = 0; iLocus < nLoci; ++iLocus){
+		for (size_t iByte = 0; iByte < bedLocusSize; ++iByte){
+			std::cout << std::bitset<byteSize>( static_cast<uint8_t>(bedBytes[iLocus * bedLocusSize + iByte]) ) << " ";
+		}
+		std::cout << "\n";
+	}
+	for (size_t iLocus = 0; iLocus < nLoci; ++iLocus){
+		std::vector<uint8_t> binLocus(binLocusSize, 0);
+		binarizeBedLocus(iLocus * bedLocusSize, bedLocusSize, bedBytes, 0, prng, binLocus);
+		for (const auto binByte : binLocus){
+			std::cout << std::bitset<byteSize>( static_cast<uint8_t>(binByte) ) << " ";
+		}
+		std::cout << "\n";
+	}
 } 
 
